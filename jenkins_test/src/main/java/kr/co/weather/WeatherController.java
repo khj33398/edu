40f923dc2ebd9f3fa80667c2f558a0a6cd702831
khj33 <khj33398@naver.com>
2022-04-11 19:46:32 +0900

    사용되지 않는 패키지 제거

--- jenkins_test/src/main/java/kr/co/weather/WeatherController.java
+++ jenkins_test/src/main/java/kr/co/weather/WeatherController.java
@@ -1,43 +1,23 @@
 package kr.co.weather;
 
-import java.io.BufferedReader;
-import java.io.File;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.ibatis.binding.MapperMethod.ParamMap;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import kr.co.weather.dao.Mapper;
 import kr.co.weather.domain.LocGrid;
-import kr.co.weather.domain.Location;
 import kr.co.weather.domain.Weather;
 import kr.co.weather.service.WeatherService;
 
